92ae1a43a82198d6e83510575d23da77327af978
norv1 <kelven.fellipe1@outlook.com>
2024-09-11 03:38:41 -0300

    admin page shows database

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,34 +1,39 @@
 "use client"
-import SauriaList from "@/components/SauriaList"
+
+import { sauria } from "@/supabase/schema"
+import { getTableConfig } from "drizzle-orm/pg-core"
+import { trpc } from "../_trpc/client"
 
 export default function admin() {
-  return <SauriaList />
-}
+  const columns = getTableConfig(sauria)
+  const sauriaList = trpc.getSauria.useQuery()
 
-//     <div className="p-5 text-sm">
-//       <table className="text-center grid">
-//         <thead>
-//           <tr className="border border-white justify-between ">
-//             {columns.columns.map(item => (
-//               <th>{item.name}</th>
-//             ))}
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {Object.values(users).map(item => (
-//             <tr key={item.id} className="border-b border-white w-96">
-//               <td>{item.arguments}</td>
-//               <td>{item.genus}</td>
-//               <td>{item.species.join(", ")}</td>
-//               <td>{item.temporal}</td>
-//               <td className="max-w-[400px] truncate">{item.img}</td>
-//               <td>{item.family}</td>
-//               <td>{item.type}</td>
-//               <td>{item.description}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   )
-// }
+  return (
+    <div className="p-5 text-sm grid">
+      <table className="text-left">
+        <thead>
+          <tr className="border-y border-white justify-between ">
+            {columns.columns.map(item => (
+              <th key={item.name}>{item.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sauriaList.data &&
+            sauriaList.data.map(item => (
+              <tr key={item.id} className="border-b border-white w-96">
+                <td>{item.type}</td>
+                <td>{item.genus}</td>
+                <td>{item.species.join(", ")}</td>
+                <td>{item.temporal}</td>
+                <td className="max-w-[400px] truncate">{item.img}</td>
+                <td>{item.family}</td>
+                <td>{item.type}</td>
+                <td>{item.description}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
