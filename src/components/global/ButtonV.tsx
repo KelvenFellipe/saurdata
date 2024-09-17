8322204f4e338299537ded3c43dc3302201803f4
norv1 <kelven.fellipe1@outlook.com>
2024-09-16 22:14:27 -0300

    Create ButtonV.tsx

---
+++ src/components/global/ButtonV.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Trash2 } from "lucide-react"
+import { useState } from "react"
+import { ButtonComponent2 } from "./ButtonComponent"
+
+interface props {
+  task: any
+}
+export function ButtonV({ task }: props) {
+  const [open, setOpen] = useState(false)
+  function handle() {
+    {
+      task()
+    }
+    setOpen(() => false)
+  }
+  return (
+    <div className="relative">
+      <ButtonComponent2 Icon1={Trash2} Click={() => setOpen(() => true)} />
+      {open === true && (
+        <div>
+          <div className="bg-zinc-800 rounded-lg p-2 items-center flex flex-col space-y-2 right-0 absolute z-50">
+            <p>Delete?</p>
+            <div className=" flex space-x-2">
+              <button
+                className="p-2 hover:bg-zinc-700/50 rounded-lg w-[50px] text-red-500"
+                onClick={handle}
+              >
+                Yes
+              </button>
+              <button
+                className="p-2 hover:bg-zinc-700/50 rounded-lg w-[50px]"
+                onClick={() => setOpen(() => false)}
+              >
+                No
+              </button>
+            </div>
+          </div>
+          <div
+            className="fixed w-full h-full top-0 left-0"
+            onClick={() => setOpen(() => false)}
+          ></div>
+        </div>
+      )}
+    </div>
+  )
+}
