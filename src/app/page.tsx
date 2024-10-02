191123aebc96a9c5f08b5ad2ead08f1644515f79
norv1 <kelven.fellipe1@outlook.com>
2024-10-02 13:59:58 -0300

    feat: added LastAdded component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+import { LastAdded } from "@/components/gallery/LastAdded"
+
 function Main() {
-  return <div className="h-max grid grid-cols-3 items-center justify-items-center"></div>
+  return (
+    <div className="h-max grid grid-cols-3 items-center justify-items-center">
+      <div className="col-span-2"></div>
+      <div className="mt-10">
+        <LastAdded />
+      </div>
+    </div>
+  )
 }
 export default Main
