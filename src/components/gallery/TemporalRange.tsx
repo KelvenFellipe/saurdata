aa31a1bdb5f556bcb42f90baf1cef6a910f99c3f
norv1 <kelven.fellipe1@outlook.com>
2024-10-14 21:55:56 -0300

    feat: updated component

--- src/components/gallery/TemporalRange.tsx
+++ src/components/gallery/TemporalRange.tsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react"
+
 export function TemporalRange({ age }: { age: string }) {
-  const my = age.split("-")
-  const position = my.map(function (x) {
-    return parseInt(x)
-  })
+  const [where, setWhere] = useState(0)
+
+  useEffect(() => {
+    if (age.includes("-")) {
+      const my = age.split("-")
+      const position = my.map(function (x) {
+        return parseInt(x)
+      })
+      setWhere(position[0])
+    } else {
+      setWhere(parseInt(age))
+    }
+  }, [])
+
   return (
-    <div className="w-fit h-8 flex rounded-md overflow-hidden relative">
-      <div className={`absolute w-[2px] h-8 bg-[#ffd900] `} style={{ right: position[0] }} />
+    <div className="w-fit h-10 my-2 flex relative select-none text-xs">
+      <div className={`absolute w-[4px] h-10 bg-[#ffd413] rounded-sm`} style={{ right: where }} />
+      <div className={`absolute w-[2px] h-10 bg-[#ff0909] rounded-sm`} style={{ right: 66 }}></div>
+      <div className={`absolute w-[2px] h-10 bg-[#ff0909] rounded-sm`} style={{ left: 0 }}></div>
+
       <div className="w-[186px] flex text-center items-center">
-        <div className="w-[51px] bg-zinc-600">T</div>
-        <div className="w-[56px] bg-zinc-500">J</div>
-        <div className="w-[79px] bg-zinc-400">C</div>
+        <div className="w-[51px] flex h-8 items-center justify-center bg-zinc-600 ">triassic</div>
+        <div className="w-[56px] flex h-8 items-center justify-center bg-zinc-500">jurassic</div>
+        <div className="w-[79px] flex h-8 items-center justify-center bg-zinc-400">cretaceous</div>
       </div>
-      <div className="w-[66px] bg-green-600 flex justify-center items-center">C</div>
+      <div className="w-[66px]  flex text-center items-center">
+        <div className="w-[43px] flex h-8 items-center justify-center bg-orange-500">P</div>
+        <div className="w-[23px] flex h-8 items-center justify-center bg-orange-600">N</div>
+      </div>
+      <div className="absolute text-xs flex -top-[16px]" style={{ right: where - 8 }}>
+        <p className="w-fit">{where}</p>
+      </div>
+      <div className="absolute text-xs right-[59px] text-red-600 top-10">66</div>
+      <div className="absolute text-xs -left-[10px] text-red-600 top-10">252</div>
     </div>
   )
 }
