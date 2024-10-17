9e74bc98b3c58e67da2bbac62427f5ab2397de64
norv1 <kelven.fellipe1@outlook.com>
2024-10-16 23:37:54 -0300

    feat: changed how information is displayed

--- src/components/gallery/TemporalRange.tsx
+++ src/components/gallery/TemporalRange.tsx
@@ -1,3 +1,4 @@
+import { CornerLeftDown, MoveLeft, MoveRight } from "lucide-react"
 import { useEffect, useState } from "react"
 
 export function TemporalRange({ age }: { age: string }) {
@@ -44,33 +45,55 @@ export function TemporalRange({ age }: { age: string }) {
   }
   return (
     <div className="space-y-3">
-      <p>{when}</p>
-      <p>{age} million years</p>
+      <p>
+        {when}, {age} Ma
+      </p>
 
-      <div className="w-fit h-10 my-2 flex relative select-none text-xs">
-        <div className={`absolute w-[4px] h-10 bg-[#ffd413] rounded-sm`} style={{ right: where }} />
+      <div className="w-fit h-20 my-2 flex relative select-none text-xs items-center">
+        <div className="flex absolute top-0 " style={{ left: 252 - where - 8 }}>
+          <CornerLeftDown className={`flex size-5 rounded-sm justify-end mt-1`} />
+          <p className="w-[50px]">{where + " Ma"}</p>
+        </div>
+
         <div
-          className={`absolute w-[2px] h-10 bg-[#ff0909] rounded-sm`}
-          style={{ right: 66 }}
-        ></div>
-        <div className={`absolute w-[2px] h-10 bg-[#ff0909] rounded-sm`} style={{ left: 0 }}></div>
+          className={`absolute flex flex-col justify-center items-center top-6 right-0`}
+          style={{ right: 66 - 7 }}
+        >
+          <div className="w-[2px] h-8 bg-[#ff0909] rounded-sm"></div>
+          <p className="text-red-600">66</p>
+        </div>
 
-        <div className="w-[186px] flex text-center items-center">
-          <div className="w-[51px] flex h-8 items-center justify-center bg-zinc-600 ">triassic</div>
-          <div className="w-[56px] flex h-8 items-center justify-center bg-zinc-500">jurassic</div>
-          <div className="w-[79px] flex h-8 items-center justify-center bg-zinc-400">
-            cretaceous
+        <div
+          className={`absolute flex flex-col justify-center items-center top-6 right-0`}
+          style={{ right: 252 - 10 }}
+        >
+          <div className="w-[2px] h-8 bg-[#ff0909] rounded-sm"></div>
+          <p className="text-red-600">252</p>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="flex text-center ">
+            <div className="w-[51px] flex h-6 items-center justify-center bg-zinc-600 ">
+              triassic
+            </div>
+            <div className="w-[56px] flex h-6 items-center justify-center bg-zinc-500">
+              jurassic
+            </div>
+            <div className="w-[79px] flex h-6 items-center justify-center bg-zinc-400">
+              cretaceous
+            </div>
+          </div>
+          <div className="w-[186px] flex justify-center h-1 bg-green-900 pt-1">
+            <MoveLeft className="size-4" />
+            <p>mesozoic</p>
+            <MoveRight className="size-4" />
           </div>
         </div>
+
         <div className="w-[66px]  flex text-center items-center">
           <div className="w-[43px] flex h-8 items-center justify-center bg-orange-500">P</div>
           <div className="w-[23px] flex h-8 items-center justify-center bg-orange-600">N</div>
         </div>
-        <div className="absolute text-xs flex -top-[16px]" style={{ right: where - 8 }}>
-          <p className="w-fit">{where}</p>
-        </div>
-        <div className="absolute text-xs right-[59px] text-red-600 top-10">66</div>
-        <div className="absolute text-xs -left-[10px] text-red-600 top-10">252</div>
       </div>
     </div>
   )
