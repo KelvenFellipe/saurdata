528dae7aadf070d57b285d238200121dba8dac5b
norv1 <kelven.fellipe1@outlook.com>
2024-10-18 21:48:59 -0300

    feat: updated Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
 import { LastAdded } from "@/components/gallery/LastAdded"
+import { MiniSaurCard } from "@/components/gallery/MiniSaurCard"
+import { trpc } from "@/connection/client/client"
+import { SaurType } from "@/types/saurType"
+import { useEffect, useState } from "react"
 
 function Main() {
+  const { data = [], isFetched, isLoading } = trpc.getSauria.useQuery()
+  const [dataSauria, setDataSauria] = useState<Array<SaurType>>([])
+  useEffect(() => {
+    setDataSauria(() => data)
+  }, [isFetched])
   return (
-    <div className="h-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-items-center">
-      <div className="col-span-2"></div>
-      <div className="mt-10 hidden md:flex">
+    <div className="h-max grid grid-cols-3 md:grid-cols-2 lg:grid-cols-5 justify-items-center">
+      <div className=""></div>
+      <div className="col-span-3 max-w-[800px] ">
+        {dataSauria.map(dino => (
+          <div>
+            <MiniSaurCard {...dino} />
+          </div>
+        ))}
+      </div>
+      <div className=" justify-end mt-6 hidden md:flex">
         <LastAdded />
       </div>
     </div>
