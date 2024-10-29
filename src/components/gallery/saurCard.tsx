0f2c1d1334a771009733c375415614852d32ea17
norv1 <kelven.fellipe1@outlook.com>
2024-10-29 16:13:21 -0300

    feat: added description in mobile

--- src/components/gallery/saurCard.tsx
+++ src/components/gallery/saurCard.tsx
@@ -2,36 +2,34 @@ import { trpc } from "@/connection/client/client"
 import { SaurType } from "@/types/saurType"
 import { useEffect, useState } from "react"
 import { Loading } from "../global/Loading"
-import { ImageOVerlay } from "./ImageOverlay"
+import { MenuSection } from "../navbar/MenuSection"
+import { ImageCarousel } from "./ImageCarousel"
 import { TemporalRange } from "./TemporalRange"
 
 export function SaurCard({ props }: any) {
   const { data = [], isFetched, isLoading } = trpc.getSauriaByGenus.useQuery(props)
   const [result, setResult] = useState<SaurType>()
-  const [open, setOpen] = useState(false)
-  const [imgs, setImgs] = useState("")
+  const [show, setShow] = useState(false)
 
   useEffect(() => {
     setResult(() => data[0])
   }, [isFetched])
-  console.log(result?.added)
+
   if (isLoading) {
     return <Loading />
   }
   if (result !== null && result !== undefined) {
     return (
-      <div className="grid grid-cols-3 rounded-xl m-auto text-white p-4 text-lg relative mx-40 space-x-10">
-        <p className="mt-3 flex justify-stretch h-max col-span-2">{result.description}</p>
-        <div className="bg-background p-4 rounded-xl w-fit">
-          <div className="flex flex-col    ml-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 lg:mx-20 rounded-xl text-white p-4 text-lg relative lg:space-x-10">
+        <p className="mt-3 justify-stretch h-max col-span-2 hidden lg:flex">{result.description}</p>
+        <div className="bg-background p-4 rounded-xl w-fit col-span-1 ">
+          <div className="flex flex-col ml-4">
             <div className="space-y-3">
               <p className="text-2xl">
                 {result.genus.charAt(0).toUpperCase() + result.genus.slice(1)}
               </p>
               <p className="">{result.family.charAt(0).toUpperCase() + result.family.slice(1)}</p>
-              <div className="ml-2">
-                <TemporalRange age={result.temporal} />
-              </div>
+              <TemporalRange age={result.temporal} />
             </div>
 
             <div className="flex flex-col text-start space-y-2">
@@ -45,36 +43,22 @@ export function SaurCard({ props }: any) {
               </div>
             </div>
           </div>
-
-          <div key={result.id} className="flex flex-col cursor-pointer w-[400px] mt-2">
-            {result.img.includes(", ") ? (
-              result.img
-                .split(", ")
-                .map(item => (
-                  <img
-                    key={item}
-                    src={item}
-                    alt={result.genus}
-                    className="rounded-md object-center w-fit h-fit transition ease-in-out duration-300"
-                    onClick={() => (setImgs(item), setOpen(() => true))}
-                  />
-                ))
-            ) : (
-              <img
-                key={result.id}
-                src={result.img}
-                alt={result.genus}
-                className="rounded-md object-center w-fit"
-                onClick={() => (setImgs(result.img), setOpen(() => true))}
-              />
-            )}
+          <div className="flex flex-col lg:hidden py-3 p-2">
+            <MenuSection name="Description">
+              <div className={``}>
+                <div className="bg-zinc-500 w-full h-[1px]"></div>
+                <div className="text-center p-2">
+                  <p className="text-start">{result.description}</p>
+                </div>
+                <div className="bg-zinc-500 w-full h-[1px]"></div>
+              </div>
+            </MenuSection>
+          </div>
+          <div className="mt-2">
+            <ImageCarousel Imgs={result.img.split(", ")} />
           </div>
         </div>
-
-        {open && <ImageOVerlay src={imgs} onClick={() => setOpen(() => false)} />}
       </div>
     )
   }
 }
-//grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
-
