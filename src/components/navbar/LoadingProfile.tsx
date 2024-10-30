35550b0c655f5ddb3df190e178fde1c964d64440
norv1 <kelven.fellipe1@outlook.com>
2024-10-30 18:32:56 -0300

    feat: added new loadings to user's avatar and notifications

--- src/components/navbar/LoadingProfile.tsx
+++ src/components/navbar/LoadingProfile.tsx
@@ -1,4 +1,18 @@
 "use client"
+export function LoadingUser() {
+  return (
+    <div className="p-[10px] flex space-x-[24px] items-center justify-end ml-auto px-2 animate-pulse">
+      <button className=" bg-zinc-300/50 dark:bg-zinc-700/50 rounded-full duration-300 h-8 w-8" />
+    </div>
+  )
+}
+export function LoadingNotification() {
+  return (
+    <div className="p-[10px] flex space-x-[24px] items-center justify-end ml-auto px-2 animate-pulse">
+      <button className=" bg-zinc-300/50 dark:bg-zinc-700/50 rounded-full duration-300 h-7 w-7" />
+    </div>
+  )
+}
 export function LoadingProfile() {
   return (
     <div className="p-[10px] flex space-x-[24px] items-center justify-end ml-auto px-2 animate-pulse">
