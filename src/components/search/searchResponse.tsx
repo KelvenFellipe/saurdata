bf37a05b3bbcc4ded4b39b0911b9cd5d6e610616
norv1 <kelven.fellipe1@outlook.com>
2024-11-03 20:32:29 -0300

    feat: code was updated to work better

--- src/components/search/searchResponse.tsx
+++ src/components/search/searchResponse.tsx
@@ -1,22 +1,16 @@
 import { SaurType } from "@/types/saurType"
 import { Search } from "lucide-react"
-import Link from "next/link"
+import { useRouter } from "next/navigation"
 
-export const SearchResponse = ({ data, click }: { data: SaurType; click: any }) => {
+export const SearchResponse = ({ data, onClick }: { data: SaurType; onClick: any }) => {
+  const router = useRouter()
   return (
-    <div>
-      <div className="flex p-1 px-3 items-center text-sm text-zinc-200 space-x-2 hover:bg-zinc-700/50 dark:hover:text-white">
-        <Link
-          href={`/gallery/${data.genus}`}
-          onClick={click}
-          className="flex transition ease-in-out items-center"
-        >
-          <div className="flex space-x-2 items-center">
-            <Search className="size-5" />
-            <p>{data.genus}</p>
-          </div>
-        </Link>
-      </div>
+    <div
+      className="flex  px-3 py-2 items-center ease-in-out cursor-pointer duration-500 text-sm text-zinc-200 space-x-2 hover:bg-zinc-700/50 dark:hover:text-white"
+      onClick={() => (router.push(`/gallery/${data.genus}`), onClick())}
+    >
+      <Search className="size-5" />
+      <p>{data.genus}</p>
     </div>
   )
 }
